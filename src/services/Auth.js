49d9cdc73961e7b8ff22b11a35b8dd9a909761f6
Emily Byrne <embyrne13@gmail.com>
2022-09-07 15:41:40 -0400

    services folder

---
+++ src/services/Auth.js
@@ -0,0 +1,29 @@
+import Client from './api'
+
+export const LoginUser = async (data) => {
+  try {
+    const res = await Client.post('/api/user/login', data)
+    localStorage.setItem('token', res.data.token)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const SignUpUser = async (data) => {
+  try {
+    const res = await Client.post('/api/user/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CheckLogin = async () => {
+  try {
+    const res = await Client.get('/api/user/loggedin')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
