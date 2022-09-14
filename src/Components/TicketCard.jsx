4c84fe23caf7b7765fbf8e2babea61f9b102eb33
Emily Byrne <embyrne13@gmail.com>
2022-09-14 10:38:31 -0400

    delete trip

--- src/Components/TicketCard.jsx
+++ src/Components/TicketCard.jsx
@@ -1,12 +1,45 @@
 import axios from 'axios'
 import { useState } from 'react'
 import { BASE_URL } from '../services/api'
+import swal from 'sweetalert'
+
 const TicketCard = ({ ticket, getTicket }) => {
   const [delT, setDelT] = useState({})
   const deleteTicket = async () => {
-    const res = await axios.delete(`${BASE_URL}/api/ticket/${ticket.id}`)
+    const res = await axios.delete(`${BASE_URL}/api/ticket/${ticket.id}`, {
+      data: {
+        id: ticket.id
+      }
+    })
+    swal('Trip has been cancelled!', {
+      icon: 'success'
+    })
     setDelT(res.data)
     getTicket()
+  }
+  const handleDeleteClick = () => {
+    swal({
+      title: 'Are you sure?',
+      text: 'Once deleted, you will not be able to recover this ticket.',
+      icon: 'warning',
+      dangerMode: true,
+      buttons: {
+        decline: {
+          text: 'Cancel',
+          value: false
+        },
+        accept: {
+          text: 'Delete',
+          value: true
+        }
+      }
+    }).then((value) => {
+      if (value) {
+        deleteTicket()
+      } else {
+        swal('Trip is not cancelled. Click OK to continue.')
+      }
+    })
   }
   return (
     <div className="ticket">
@@ -22,7 +55,7 @@ const TicketCard = ({ ticket, getTicket }) => {
       <h2 className="ticketInfo">
         Cost: <p className="tI">${ticket.flightFare}</p>
       </h2>
-      <button className="buttonD" onClick={() => deleteTicket(ticket.id)}>
+      <button className="buttonD" onClick={handleDeleteClick}>
         Cancel Trip
       </button>
     </div>
